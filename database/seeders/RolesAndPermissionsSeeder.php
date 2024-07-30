233e07bfb1c39e5ec6684dde1595ce489e6e141c
Daniel <daniel.cabane@gmail.com>
2024-07-30 23:36:11 +0800

    Adding Hash facade to seeder

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\App;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
