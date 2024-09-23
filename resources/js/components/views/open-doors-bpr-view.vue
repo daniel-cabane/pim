4e2fed902ba00e52ee4a785a8f627adc524a3241
Daniel <daniel.cabane@gmail.com>
2024-09-23 22:50:08 +0800

    more testing

--- resources/js/components/views/open-doors-bpr-view.vue
+++ resources/js/components/views/open-doors-bpr-view.vue
@@ -1,6 +1,9 @@
 <template>
     <v-container>
         <div class="fullscreen">
+            <div style="position:absolute;top:0px;left:0px;">
+                <v-icon icon="mdi-circle" v-for="led in leds" :color="led ? 'success' : 'surface'"/>
+            </div>
             <div v-if="message.length">
                 Message : {{ message }}
             </div>
@@ -17,19 +20,23 @@
     </v-container>
 </template>
 <script setup>
-    import { ref } from "vue";
+    import { ref, reactive } from "vue";
 
     const loading = ref(false);
     const message = ref('');
     const serialNumber = ref('');
     const error = ref('');
 
+    let leds = reactive([false, false, false, false]);
+
 
     const scanTag = async () => {
-        console.log("Scanning");
+        leds = [false, false, false, false];
+        leds[0] = true;
         loading.value = true;
         try {
             const ndef = new NDEFReader();
+            leds[1] = true;
             await ndef.scan();
 
             ndef.addEventListener("readingerror", () => {
@@ -39,9 +46,12 @@
             ndef.addEventListener("reading", ({ msg, serial }) => {
                 message.value = msg;
                 serialNumber.value = serial;
+                leds[2] = true;
             });
             loading.value = false;
         } catch (err) {
+            leds[3] = true;
+            console.error(err);
             error.value = err;
             loading.value = false;
         }
@@ -53,5 +63,6 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        position: relative;
     }
 </style>
