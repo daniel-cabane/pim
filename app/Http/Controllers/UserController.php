7c91233a5e5b43aa8d306289ec3e1fcc5a1ea33b
Daniel <daniel.cabane@gmail.com>
2024-11-04 22:51:53 +0800

    another try

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -94,7 +94,7 @@ class UserController extends Controller
 
   public function googleSigninCallback(Request $request)
   {
-    logger($request);
+    logger($request->query('workshop'));
     $attrs = $request->validate(['workshop' => 'sometimes|integer']);
     logger("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
     logger($attrs);
